a213b6b45eef473bfcdfe99fec964819cf64ad95
CriisDevEnterp <cristian.delarosa@neoris.com>
2024-05-31 11:46:22 -0600

    [ADD] Implementación con JWT

--- src/main/java/com/cristiand/practica/springboot/app/springboot_practica_assassins/dao/UserRepository.java
+++ src/main/java/com/cristiand/practica/springboot/app/springboot_practica_assassins/dao/UserRepository.java
@@ -1,10 +1,15 @@
 package com.cristiand.practica.springboot.app.springboot_practica_assassins.dao;
 
 import com.cristiand.practica.springboot.app.springboot_practica_assassins.entity.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username); 
     
 }
