4d8249bc55457fcae3b2109a7a8f95d93def6911
CriisDevEnterp <cristian.delarosa@neoris.com>
2024-07-12 12:25:08 -0600

    [MOD] Modificación de pruebas.

--- src/test/java/com/cristiand/practica/springboot/app/springboot_practica_assassins/service/TokenServiceImplTest.java
+++ src/test/java/com/cristiand/practica/springboot/app/springboot_practica_assassins/service/TokenServiceImplTest.java
@@ -4,13 +4,16 @@ import com.cristiand.practica.springboot.app.springboot_practica_assassins.dao.U
 import com.cristiand.practica.springboot.app.springboot_practica_assassins.dto.LoginRequest;
 import com.cristiand.practica.springboot.app.springboot_practica_assassins.dto.LoginResponse;
 import com.cristiand.practica.springboot.app.springboot_practica_assassins.entity.User;
+import io.jsonwebtoken.lang.Collections;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -30,8 +33,12 @@ public class TokenServiceImplTest {
         // Configurar datos de prueba
         User user = new User();
         user.setUsername("username");
-        user.setEnabled(true);
         user.setPassword(passwordEncoder.encode("password"));
+        user.setEnabled(true);
+        user.setFirstName("testFirstname");
+        user.setLastName("testLastname");
+        user.setEmail("test@prueba.com");
+        user.setRoles(Collections.emptyList());
         userRepository.save(user);
 
         LoginRequest loginRequest = new LoginRequest("username", "password");
@@ -44,10 +51,14 @@ public class TokenServiceImplTest {
     @Test
     public void testLoginFailureUserNotFound() {
         LoginRequest loginRequest = new LoginRequest("nonexistentuser", "password");
-        LoginResponse loginResponse = tokenService.login(loginRequest);
 
-        assertEquals("", loginResponse.accessToken());
-        assertEquals(0L, loginResponse.accessTokenExpiresIn());
+        // Verificar que se lanza la excepción correcta
+        BadCredentialsException thrown = assertThrows(
+                BadCredentialsException.class,
+                () -> tokenService.login(loginRequest),
+                "Expected login() to throw, but it didn't");
+
+        assertEquals("Credenciales no válidas.", thrown.getMessage());
     }
 
     @Test
@@ -55,15 +66,22 @@ public class TokenServiceImplTest {
         // Configurar datos de prueba
         User user = new User();
         user.setUsername("existinguser");
-        user.setEnabled(true);
         user.setPassword(passwordEncoder.encode("correctpassword"));
+        user.setEnabled(true);
+        user.setFirstName("testFirstname");
+        user.setLastName("testLastname");
+        user.setEmail("test@prueba.com");
         userRepository.save(user);
 
         LoginRequest loginRequest = new LoginRequest("existinguser", "incorrectpassword");
-        LoginResponse loginResponse = tokenService.login(loginRequest);
 
-        assertEquals("", loginResponse.accessToken());
-        assertEquals(0L, loginResponse.accessTokenExpiresIn());
+        // Verificar que se lanza la excepción correcta
+        BadCredentialsException thrown = assertThrows(
+                BadCredentialsException.class,
+                () -> tokenService.login(loginRequest),
+                "Expected login() to throw, but it didn't");
+
+        assertEquals("Credenciales no válidas.", thrown.getMessage());
     }
-    
+
 }
